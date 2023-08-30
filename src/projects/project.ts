493c1c5c3c8b8e9762e756c407cc3733ddffe777
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-30 19:17:25 +0530

    move to projects folder.

--- src/projects/project.ts
+++ src/projects/project.ts
@@ -1,6 +1,6 @@
 import {makeProject} from '@motion-canvas/core';
 
-import bubble_sort from "./scenes/bubble_sort?scene";
+import bubble_sort from "../scenes/bubble_sort?scene";
 
 export default makeProject({
   scenes: [bubble_sort],
