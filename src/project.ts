b98f7aada38ca7aa13f9017e5dfd3f3670bd76e6
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-30 15:10:12 +0530

    move to scenes folder.

--- src/project.ts
+++ src/project.ts
@@ -1,6 +1,6 @@
 import {makeProject} from '@motion-canvas/core';
 
-import looping_rectangles from "../looping_rectangles?scene";
+import looping_rectangles from "./scenes/looping_rectangles?scene";
 
 export default makeProject({
   scenes: [looping_rectangles],
