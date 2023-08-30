9438eebbc1e552bda614c5adc48876e14021d02e
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-30 17:26:24 +0530

    add logic to generate random number list.

--- src/scenes/bubble_sort.tsx
+++ src/scenes/bubble_sort.tsx
@@ -1,5 +1,10 @@
 import {makeScene2D} from "@motion-canvas/2d";
+import {range, useRandom} from "@motion-canvas/core";
 
 export default makeScene2D(function* (view) {
     view.fill('#141414');
+
+    const randomNumbersListLength = 10;
+    const random = useRandom();
+    let randomNumbers = range(randomNumbersListLength).map(_ =>random.nextInt(1, 100));
 });
