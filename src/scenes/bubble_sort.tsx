2da160993a89e60bc55e3b91bf63ff32f1977be6
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-30 17:27:47 +0530

    add a comment.

--- src/scenes/bubble_sort.tsx
+++ src/scenes/bubble_sort.tsx
@@ -4,6 +4,7 @@ import {range, useRandom} from "@motion-canvas/core";
 export default makeScene2D(function* (view) {
     view.fill('#141414');
 
+    // random number list generation
     const randomNumbersListLength = 10;
     const random = useRandom();
     let randomNumbers = range(randomNumbersListLength).map(_ =>random.nextInt(1, 100));
