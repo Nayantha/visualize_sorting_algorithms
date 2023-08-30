67f6c28b9eb122671ae71ed0c1de21ce5bf1d05a
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-30 14:30:22 +0530

    add circle to view.

--- src/scenes/example.tsx
+++ src/scenes/example.tsx
@@ -1,8 +1,18 @@
-import {makeScene2D} from '@motion-canvas/2d';
-import {waitFor} from '@motion-canvas/core';
+import {Circle, makeScene2D} from '@motion-canvas/2d';
+import {createRef, waitFor} from '@motion-canvas/core';
 
 export default makeScene2D(function* (view) {
-  // Create your animations here
+    const myCircle = createRef<Circle>()
 
-  yield* waitFor(5);
+  view.add(
+      <Circle
+          ref={myCircle}
+          x={-300}
+          width={140}
+          height={140}
+          fill="#e13238"
+      />
+  );
+
+    yield* waitFor(5);
 });
