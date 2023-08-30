b6be7361a9ac734e86dbf31ae350b0bb3f971974
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-30 17:30:26 +0530

    generate a signal list from the random number list.

--- src/scenes/bubble_sort.tsx
+++ src/scenes/bubble_sort.tsx
@@ -1,5 +1,5 @@
 import {makeScene2D} from "@motion-canvas/2d";
-import {range, useRandom} from "@motion-canvas/core";
+import {createSignal, range, useRandom} from "@motion-canvas/core";
 
 export default makeScene2D(function* (view) {
     view.fill('#141414');
@@ -8,4 +8,7 @@ export default makeScene2D(function* (view) {
     const randomNumbersListLength = 10;
     const random = useRandom();
     let randomNumbers = range(randomNumbersListLength).map(_ =>random.nextInt(1, 100));
+
+    //random number list to signals
+    const signals = randomNumbers.map(number=>createSignal(number));
 });
