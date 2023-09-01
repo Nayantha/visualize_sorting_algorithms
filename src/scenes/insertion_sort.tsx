8f163ded6da078b51d2f1f9a4974d94b3bf7c3eb
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-01 20:53:36 +0530

    replace the random number list with a signal list.

--- src/scenes/insertion_sort.tsx
+++ src/scenes/insertion_sort.tsx
@@ -1,11 +1,11 @@
 import {makeScene2D} from "@motion-canvas/2d";
-import {range, useRandom} from "@motion-canvas/core";
+import {createSignal, range, useRandom} from "@motion-canvas/core";
 
 export default makeScene2D(function* (view) {
     view.fill('#141414');
     //<editor-fold desc="generate random number list">
     const randomNumbersListLength = 10;
     const random = useRandom();
-    let randomNumbers = range(randomNumbersListLength).map(_ => random.nextInt(1, 100));
+    const signals = range(randomNumbersListLength).map(_ => createSignal(random.nextInt(1, 100)));
     //</editor-fold>
 })
