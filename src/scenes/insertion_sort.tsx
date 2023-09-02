4e0e119c6b32a8c58bc8818a698adc4f02919c12
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-02 19:16:35 +0530

    fix generator error.

--- src/scenes/insertion_sort.tsx
+++ src/scenes/insertion_sort.tsx
@@ -88,7 +88,7 @@ export default makeScene2D(function* (view) {
         let search_index = i - 1;
         yield* code().selection(lines(2), 0.2);
         while (search_index >= 0 && text_list[map.get(i)].text() < text_list[map.get(search_index)].text()) {
-            yield code().selection(lines(3, 5), 0.2);
+            yield* code().selection(lines(3, 5), 0.2);
             search_index -= 1;
         }
         let temp = map.get(i);
