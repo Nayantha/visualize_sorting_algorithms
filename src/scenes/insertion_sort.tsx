d4cbc2046ef54299dc9713b057a1ba758220dd77
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-02 20:08:45 +0530

    remove the sorting algorithm logic.

--- src/scenes/insertion_sort.tsx
+++ src/scenes/insertion_sort.tsx
@@ -1,5 +1,5 @@
 import {makeScene2D, Rect, Txt} from "@motion-canvas/2d";
-import {all, createRef, createSignal, DEFAULT, makeRef, range, useRandom} from "@motion-canvas/core";
+import {createRef, createSignal, DEFAULT, makeRef, range, useRandom} from "@motion-canvas/core";
 import {CodeBlock, lines} from "@motion-canvas/2d/lib/components/CodeBlock";
 
 export default makeScene2D(function* (view) {
@@ -84,41 +84,7 @@ export default makeScene2D(function* (view) {
     }
     //</editor-fold>
     const jump = 175;
-    for (let i = 1; i < randomNumbersListLength; i++) {
-        yield* all(
-            code().selection(lines(1), 0.2),
-            rectangle_list[i].y(rectangle_list[i].y() + jump, 0.1),
-            text_list[i].y(rectangle_list[i].y() + jump, 0.1),
-        );
-        let search_index = i - 1;
-        yield* all(
-            code().selection(lines(2), 0.2),
-            rectangle_list[search_index].fill("#e6a700", 0.2),
-        );
-        while (search_index >= 0 && text_list[map.get(i)].text() < text_list[map.get(search_index)].text()) {
-            const current_search_element_x_coordinate = rectangle_list[search_index].x();
-            yield* all(
-                code().selection(lines(3, 5), 0.2),
-                rectangle_list[search_index].x(rectangle_list[i].x(), 0.1),
-                text_list[search_index].x(text_list[i].x(), 0.1),
-                rectangle_list[i].x(current_search_element_x_coordinate, 0.1),
-                text_list[i].x(current_search_element_x_coordinate, 0.1),
-            );
-            search_index -= 1;
-            yield* code().selection(lines(6), 0.2);
-        }
-        let temp = map.get(i);
-        map.set(i, search_index + 1)
-        map.set(search_index + 1, temp)
-        yield* all(
-            code().selection(lines(7, 10), 0.2),
-            rectangle_list[i].y(rectangle_list[i].y() - jump, 0.1),
-            text_list[i].y(rectangle_list[i].y() - jump, 0.1),
-            );
-        for (let i = 0; i < randomNumbersListLength; i++) {
-            yield* rectangle_list[i].fill("#e3242b", 0.05);
-        }
-    }
+
     yield* code().selection(DEFAULT, 0.2);
     for (let k = 0; k < randomNumbersListLength; k++) {
         yield* rectangle_list[k].fill('#2be324', 0.15);
