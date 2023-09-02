ef0318eb03dc7eee3979465fbe328a3cc1b34e7b
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-02 16:52:29 +0530

    add rectangles to that wrap the random number string.

--- src/scenes/insertion_sort.tsx
+++ src/scenes/insertion_sort.tsx
@@ -1,5 +1,5 @@
-import {makeScene2D} from "@motion-canvas/2d";
-import {createSignal, range, useRandom} from "@motion-canvas/core";
+import {makeScene2D, Rect} from "@motion-canvas/2d";
+import {createSignal, makeRef, range, useRandom} from "@motion-canvas/core";
 
 export default makeScene2D(function* (view) {
     view.fill('#141414');
@@ -7,5 +7,21 @@ export default makeScene2D(function* (view) {
     const randomNumbersListLength = 10;
     const random = useRandom();
     const signals = range(randomNumbersListLength).map(_ => createSignal(random.nextInt(1, 100)));
+    //</editor-fold>
+    //<editor-fold desc="Add Rectangles to wrap the random numbers">
+    const rectangle_list: Rect[] = [];
+    view.add(
+        range(randomNumbersListLength).map(i => (
+            <Rect
+                ref={makeRef(rectangle_list, i)}
+                width={150}
+                height={150}
+                x={-185 * (randomNumbersListLength - 1) / 2 + 185 * i}
+                fill="#e3242b"
+                radius={10}
+                y={-275}
+            />
+        ))
+    )
     //</editor-fold>
 })
