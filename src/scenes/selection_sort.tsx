be7bc1e311a1e8da2f0bd1d0c6e8e6dbd5ddcc2a
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-05 22:37:53 +0530

    remove unwanted code for this file.

--- src/scenes/selection_sort.tsx
+++ src/scenes/selection_sort.tsx
@@ -1,5 +1,5 @@
 import {makeScene2D, Rect, Txt} from "@motion-canvas/2d";
-import {all, createRef, createSignal, DEFAULT, makeRef, range, useRandom} from "@motion-canvas/core";
+import {createRef, createSignal, DEFAULT, makeRef, range, useRandom} from "@motion-canvas/core";
 import {CodeBlock, lines} from "@motion-canvas/2d/lib/components/CodeBlock";
 
 export default makeScene2D(function* (view) {
@@ -83,43 +83,7 @@ export default makeScene2D(function* (view) {
         map.set(i, i);
     }
     //</editor-fold>
-    const jump = 175;
-    for (let i = 1; i < randomNumbersListLength; i++) {
-        let key = parseInt(text_list[map.get(i)].text());
-        let j = i - 1;
-        yield* all(
-            code().selection(lines(2), 0.1),
-            rectangle_list[map.get(i)].fill('#e6a700', 0.1),
-            rectangle_list[map.get(i)].y(rectangle_list[map.get(i)].y() - jump, 0.2),
-            text_list[map.get(i)].y(text_list[map.get(i)].y() - jump, 0.2),
-        );
-        yield* code().selection(lines(3), 0.2);
-        yield* code().selection(lines(4), 0.2);
-        let steps = 0;
-        while (j >= 0 && key < parseInt(text_list[map.get(j)].text())){
-            steps += 1;
-            yield* all(
-                code().selection(lines(5), 0.1),
-                rectangle_list[map.get(j)].x(rectangle_list[map.get(j)].x() + space_x, 0.2),
-                text_list[map.get(j)].x(text_list[map.get(j)].x() + space_x, 0.2),
-                rectangle_list[map.get(j + 1)].x(rectangle_list[map.get(j + 1)].x() - space_x, 0.2),
-                text_list[map.get(j + 1)].x(text_list[map.get(j + 1)].x() - space_x, 0.2),
-            );
-            let temp = map.get(j + 1);
-            map.set(j + 1, map.get(j));
-            map.set(j, temp);
-            j -= 1;
-            yield* code().selection(lines(6), 0.2);
-            yield* code().selection(lines(4), 0.2);
-        }
-        yield* code().selection(lines(7), 0.2);
-        yield* all(
-            rectangle_list[map.get(j + 1)].fill('#e3242b', 0.2),
-            rectangle_list[map.get(j + 1)].y(rectangle_list[map.get(j + 1)].y() + jump, 0.2),
-            text_list[map.get(j + 1)].y(text_list[map.get(j + 1)].y() + jump, 0.2),
-        );
-        yield* code().selection(lines(1), 0.2);
-    }
+    // const jump = 175;
     yield* code().selection(DEFAULT, 0.2);
     for (let k = 0; k < randomNumbersListLength; k++) {
         yield* rectangle_list[map.get(k)].fill('#2be324', 0.15);
