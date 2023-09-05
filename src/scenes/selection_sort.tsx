f3aacd2f1e324cf1b18b60af940776ea7a208b79
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-05 22:24:11 +0530

    add rectangle list to view.

--- src/scenes/selection_sort.tsx
+++ src/scenes/selection_sort.tsx
@@ -1,6 +1,21 @@
 import {makeScene2D, Rect} from "@motion-canvas/2d";
+import {makeRef, range} from "@motion-canvas/core";
 
 export default makeScene2D(function* (view) {
     view.fill('#141414');
+    const num_of_items_in_list = 7;
     const text_wrappers: Rect[] = [];
+    view.add(
+        range(num_of_items_in_list).map(i => (
+            <Rect
+                ref={makeRef(text_wrappers,i)}
+                width={150}
+                height={150}
+                x={-185 * (num_of_items_in_list - 1) / 2 + 185 * i}
+                fill="#e3242b"
+                radius={10}
+                y={-275}
+            />
+        ))
+    )
 })
