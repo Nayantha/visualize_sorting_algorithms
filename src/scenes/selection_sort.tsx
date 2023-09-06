c74088016ecfd354fef1f1f8bcbd9cb1e3ef1128
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-06 20:20:48 +0530

    remove step by step moving through x-axis.

--- src/scenes/selection_sort.tsx
+++ src/scenes/selection_sort.tsx
@@ -98,15 +98,22 @@ export default makeScene2D(function* (view) {
                 )
                 yield* code().selection(lines(4), 0.2);
                 //<editor-fold desc="move minimum to front and the previous minimum to the back of the array">
-                for (let k = i; k < j; k++) {
-                    yield* all(
-                        text_wrappers[map.get(i)].x(text_wrappers[map.get(i)].x() + space_x, 0.1),
-                        random_number_text_list[map.get(i)].x(random_number_text_list[map.get(i)].x() + space_x, 0.1),
-                        text_wrappers[map.get(j)].x(text_wrappers[map.get(j)].x() - space_x, 0.1),
-                        random_number_text_list[map.get(j)].x(random_number_text_list[map.get(j)].x() - space_x, 0.1),
-                    )
-                    yield* waitFor(0.2);
-                }
+                yield* all(
+                    text_wrappers[map.get(i)].x(text_wrappers[map.get(i)].x() + space_x * (j - i), 0.1),
+                    random_number_text_list[map.get(i)].x(random_number_text_list[map.get(i)].x() + space_x * (j - i), 0.1),
+                    text_wrappers[map.get(j)].x(text_wrappers[map.get(j)].x() - space_x * (j - i), 0.1),
+                    random_number_text_list[map.get(j)].x(random_number_text_list[map.get(j)].x() - space_x * (j - i), 0.1),
+                )
+                yield* waitFor(0.2);
+                // for (let k = i; k < j; k++) {
+                // yield* all(
+                //     text_wrappers[map.get(i)].x(text_wrappers[map.get(i)].x() + space_x, 0.1),
+                //     random_number_text_list[map.get(i)].x(random_number_text_list[map.get(i)].x() + space_x, 0.1),
+                //     text_wrappers[map.get(j)].x(text_wrappers[map.get(j)].x() - space_x, 0.1),
+                //     random_number_text_list[map.get(j)].x(random_number_text_list[map.get(j)].x() - space_x, 0.1),
+                // )
+                // yield* waitFor(0.2);
+                // }
                 //</editor-fold>
                 yield* all(
                     text_wrappers[map.get(i)].y(text_wrappers[map.get(i)].y() + jump, 0.1),
@@ -116,7 +123,7 @@ export default makeScene2D(function* (view) {
                 )
                 yield* waitFor(0.2);
                 yield* all(
-                text_wrappers[map.get(j)].y(text_wrappers[map.get(j)].y() - jump, 0.1),
+                    text_wrappers[map.get(j)].y(text_wrappers[map.get(j)].y() - jump, 0.1),
                     random_number_text_list[map.get(j)].y(random_number_text_list[map.get(j)].y() - jump, 0.1),
                 )
                 const temp = map.get(j)
@@ -129,7 +136,7 @@ export default makeScene2D(function* (view) {
             random_number_text_list[map.get(i)].y(random_number_text_list[map.get(i)].y() + jump, 0.1),
         );
     }
-    yield* text_wrappers[map.get(number_of_items_to_be_sorted-1)].fill("#e3242b", 0.2);
+    yield* text_wrappers[map.get(number_of_items_to_be_sorted - 1)].fill("#e3242b", 0.2);
     yield* code().selection(DEFAULT, 0.2);
     //<editor-fold desc="Convert The background Rect color to green">
     for (let k = 0; k < number_of_items_to_be_sorted; k++) {
