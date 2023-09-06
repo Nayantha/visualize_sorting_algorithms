e4e98a3e0acda79e840d074ada5d4941aefbc9d6
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-06 20:08:22 +0530

    reset the y coordinates of the i and j elements.

--- src/scenes/selection_sort.tsx
+++ src/scenes/selection_sort.tsx
@@ -105,6 +105,12 @@ export default makeScene2D(function* (view) {
                     yield* waitFor(0.2);
                 }
                 //</editor-fold>
+                yield* all(
+                    text_wrappers[map.get(i)].y(text_wrappers[map.get(i)].y() + jump, 0.1),
+                    random_number_text_list[map.get(i)].y(random_number_text_list[map.get(i)].y() + jump, 0.1),
+                    text_wrappers[map.get(j)].y(text_wrappers[map.get(j)].y() - jump, 0.1),
+                    random_number_text_list[map.get(j)].y(random_number_text_list[map.get(j)].y() - jump, 0.1),
+                )
                 const temp = map.get(j)
                 map.set(j, map.get(i))
                 map.set(i, temp);
